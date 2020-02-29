7b35475136b0841a4e51773d8cd874195320827b
Bikin Maharjan <bikinmaharjan10@gmail.com>
2020-02-29 23:18:36 +1100

    Minor fix with path

--- server.js
+++ server.js
@@ -20,11 +20,11 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/v1/transactions', transactions);
-app.use('/', serveStatic(path.join(__dirname, '/client/dist')));
+app.use('/', serveStatic(path.join(__dirname, '/dist')));
 
 // this * route is to serve project on different page routes except root `/`
 app.get(/.*/, function(req, res) {
-  res.sendFile(path.join(__dirname, '/client/dist/index.html'));
+  res.sendFile(path.join(__dirname, '/dist/index.html'));
 });
 
 const PORT = process.env.PORT || 5500;
