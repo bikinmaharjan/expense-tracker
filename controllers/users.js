be3a2e778f690b3b86a4a9a6ef851b16622d25a9
Bikin Maharjan <bikinmaharjan10@gmail.com>
2020-03-04 19:01:13 +1100

    Login implemented and JWT to store the token

--- controllers/users.js
+++ controllers/users.js
@@ -48,5 +48,28 @@ exports.register = async (req, res, next) => {
 //@route: /api/v1/login
 //@access: Public
 exports.login = async (req, res, next) => {
-  return res.send('Login route');
+  //Validate Data
+  const { error } = loginValidation.validate(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+
+  //Check if emailExist for validation
+  const user = await User.findOne({
+    email: req.body.email
+  });
+
+  if (!user) {
+    return res.status(400).send(`Email doesn't exist`);
+  }
+
+  //Check if password is correct
+  const validPass = await bcrypt.compare(req.body.password, user.password);
+  if (!validPass) {
+    return res.status(400).send(`Invalid password`);
+  }
+
+  //Create and assign a token
+  const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
+  res.header('auth-token', token).send(token);
 };
